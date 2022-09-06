93448347e5d74f64ed3331e018c97a80a9e0aeb7
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-06 03:25:39 -0500

    updated schema and add validation for schema field

--- schemas/appointment.schema.js
+++ schemas/appointment.schema.js
@@ -3,22 +3,22 @@ const { Schema, model } = require("mongoose");
 
 let appointmentSchema = new Schema({
     doctor:{type: Schema.Types.ObjectId, ref:"Doctor", required:[true, "There must be a Doctor assigned to the appointment"]},
-    visitStart: {type:Date, required: true},
+    visitStart: {type:Date, required: [true, "Please provide a visit time"]},
     guardian:{type:String},
     notes:{type:String},
-    fname: {type:String},
-    lname: {type:String},
-    email: {type:String},
-    gender: {type:String},
+    fname: {type:String, required:[true, "Please provide First Name"]},
+    lname: {type:String, required:[true, "Please provide First Name"]},
+    email: {type:String, required:[true, "Please provide Email"]},
+    gender: {
+        type:String,
+        required: [function(){
+            return !this.email
+        }, "No email detected, Please provide email"]
+     },
     phone:{type:String},
     userId: {type: Schema.Types.ObjectId},
     status: {type:Boolean, default:false},
 }, {timestamps:true});
 // Will create timestamps for the createdAt and updatedAt time;
 
-appointmentSchema.post('find', async function(documents){
-    for(let doc of documents){
-        await doc.populate('doctor',{createdAt: 0, password: 0, updatedAt: 0, isSuperAdmin:0});
-    }
-});
 module.exports = model("Appointment", appointmentSchema);
